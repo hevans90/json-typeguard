234444416db389555a86d724152450e50b429c9b
hevans90 <hevans9000@gmail.com>
2018-02-02 11:29:04 +0000

    feat(models): un-comment more models for use

--- search/search-results/models/tag.ts
+++ search/search-results/models/tag.ts
@@ -4,24 +4,24 @@ export interface Tag {
     name: string;
 }
 
-// interface DetailedTag extends Tag {
-//     module: string;
-//     children_url: string;
-//     num_children: number;
-//     descendants_url: string;
-//     num_descendants: number;
-//     is_preferable: boolean;
-//     num_preferable_descendants: number;
-//     parent?: DetailedTag;
-// }
+interface DetailedTag extends Tag {
+    module: string;
+    children_url: string;
+    num_children: number;
+    descendants_url: string;
+    num_descendants: number;
+    is_preferable: boolean;
+    num_preferable_descendants: number;
+    parent?: DetailedTag;
+}
 
-// export interface DiningTag extends Tag { }
+export interface DiningTag extends Tag { }
 
-// export interface EntertainmentTag extends DetailedTag { }
+export interface EntertainmentTag extends DetailedTag { }
 
-// export interface MemberBenefitTag extends DetailedTag { }
+export interface MemberBenefitTag extends DetailedTag { }
 
-// export interface MemberEventTag extends DetailedTag {
-//     member_benefit_category: string;
-//     member_event_category: string;
-// }
+export interface MemberEventTag extends DetailedTag {
+    member_benefit_category: string;
+    member_event_category: string;
+}
